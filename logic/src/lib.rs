813bc0d4e2f9edc0a16c3d151927084f8e6841dc
Adesh Gupta <adeshgupta101@gmail.com>
2024-12-27 13:53:37 +0530

    Initial setup

---
+++ logic/src/lib.rs
@@ -0,0 +1,312 @@
+use calimero_sdk::borsh::{BorshDeserialize, BorshSerialize};
+use calimero_sdk::env::ext::{AccountId, ProposalId};
+use calimero_sdk::serde::{Deserialize, Serialize};
+use calimero_sdk::types::Error;
+use calimero_sdk::{app, env};
+use calimero_storage::collections::{UnorderedMap, Vector};
+
+#[app::state(emits = Event)]
+#[derive(Debug, PartialEq, PartialOrd, BorshSerialize, BorshDeserialize)]
+#[borsh(crate = "calimero_sdk::borsh")]
+pub struct AppState {
+    messages: UnorderedMap<ProposalId, Vector<Message>>,
+    // Need to store which player is active currently
+    active_player: u32,
+
+}
+
+// Okay so here are defined the structs that will be used in the logic
+#[derive(
+    Clone, Debug, PartialEq, PartialOrd, BorshSerialize, BorshDeserialize, Serialize, Deserialize,
+)]
+#[borsh(crate = "calimero_sdk::borsh")]
+#[serde(crate = "calimero_sdk::serde")]
+pub struct Message {
+    id: String,
+    proposal_id: String,
+    author: String,
+    text: String,
+    created_at: String,
+}
+
+
+pub enum Suit{
+    Heart,
+    Diamond,
+    Club,
+    Spade
+}
+
+pub enum Rank{
+    Jack,
+    King,
+    Queen,
+    Ace,
+    Number(u8),
+}
+
+pub struct Card{
+    pub suit: Suit,
+    pub rank: Rank,
+}
+
+
+pub struct Player{
+    pub hand: Vector<Card>,
+    pub chips: f32,
+}
+
+pub struct Pot{
+    pub pot_size: f32,
+}
+
+
+
+
+// Here are defined the events that will be emitted by the logic
+#[app::event]
+pub enum Event {
+    ProposalCreated { id: ProposalId },
+    ApprovedProposal { id: ProposalId },
+    PlayerChanged { id: u32 },
+}
+
+
+// Here are defined the requests that will be received by the logic
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(crate = "calimero_sdk::serde")]
+pub struct CreateProposalRequest {
+    pub action_type: String,
+    pub params: serde_json::Value,
+}
+
+
+// These are the functions that will be called by the frontend
+#[app::logic]
+impl AppState {
+
+    // Constructor of the state
+    #[app::init]
+    pub fn init() -> AppState {
+        AppState {
+            messages: UnorderedMap::new(),
+            active_player: 0,
+        }
+    }
+
+    // Function to get the currently active player
+    pub fn get_active_player(&self) -> Result<u32, Error> {
+
+        Ok(self.active_player)
+    }
+
+    pub fn set_active_player(&mut self, player: u32) -> Result<u32, Error> {
+        self.active_player = player;
+
+        env::emit(&Event::PlayerChanged { id: player });
+        Ok(player)
+    }
+
+
+
+    // Make a bid of a certain amount
+    
+    // Fold the current player's hand
+
+    // Make a raise of a certain amount (should be higher than the current bid, and should be made by the current player)
+
+    // 
+
+
+
+    // Get the current player
+
+
+    // Get the current pot size
+
+
+
+    // Creating a new proposal
+    pub fn create_new_proposal(
+        &mut self,
+        request: CreateProposalRequest,
+    ) -> Result<ProposalId, Error> {
+
+        // Logs into the node?
+        env::log("Starting create_new_proposal");
+        env::log(&format!("Request type: {}", request.action_type));
+
+
+
+        let proposal_id = match request.action_type.as_str() {
+            "ExternalFunctionCall" => {
+                env::log("Processing ExternalFunctionCall");
+
+                // Parsing happens in this specific format
+                let receiver_id = request.params["receiver_id"]
+                    .as_str()
+                    .ok_or_else(|| Error::msg("receiver_id is required"))?;
+                let method_name = request.params["method_name"]
+                    .as_str()
+                    .ok_or_else(|| Error::msg("method_name is required"))?;
+                let args = request.params["args"]
+                    .as_str()
+                    .ok_or_else(|| Error::msg("args is required"))?;
+                let deposit = request.params["deposit"]
+                    .as_str()
+                    .ok_or_else(|| Error::msg("deposit is required"))?
+                    .parse::<u128>()?;
+                let gas = request.params["gas"]
+                    .as_str()
+                    .map(|g| g.parse::<u64>())
+                    .transpose()?
+                    .unwrap_or(30_000_000_000_000);
+
+                env::log(&format!(
+                    "Parsed values: receiver_id={}, method_name={}, args={}, deposit={}, gas={}",
+                    receiver_id, method_name, args, deposit, gas
+                ));
+                
+                // What specifically is external? and propose()
+                Self::external()
+                    .propose()
+                    .external_function_call(
+                        receiver_id.to_string(),
+                        method_name.to_string(),
+                        args.to_string(),
+                        deposit,
+                        gas,
+                    )
+                    .send()
+            }
+            "Transfer" => {
+                env::log("Processing Transfer");
+                let receiver_id = request.params["receiver_id"]
+                    .as_str()
+                    .ok_or_else(|| Error::msg("receiver_id is required"))?;
+                let amount = request.params["amount"]
+                    .as_str()
+                    .ok_or_else(|| Error::msg("amount is required"))?
+                    .parse::<u128>()?;
+
+                Self::external()
+                    .propose()
+                    .transfer(AccountId(receiver_id.to_string()), amount)
+                    .send()
+            }
+            "SetContextValue" => {
+                env::log("Processing SetContextValue");
+                let key = request.params["key"]
+                    .as_str()
+                    .ok_or_else(|| Error::msg("key is required"))?
+                    .as_bytes()
+                    .to_vec()
+                    .into_boxed_slice();
+                let value = request.params["value"]
+                    .as_str()
+                    .ok_or_else(|| Error::msg("value is required"))?
+                    .as_bytes()
+                    .to_vec()
+                    .into_boxed_slice();
+
+                Self::external()
+                    .propose()
+                    .set_context_value(key, value)
+                    .send()
+            }
+            "SetNumApprovals" => Self::external()
+                .propose()
+                .set_num_approvals(
+                    request.params["num_approvals"]
+                        .as_u64()
+                        .ok_or(Error::msg("num_approvals is required"))? as u32,
+                )
+                .send(),
+            "SetActiveProposalsLimit" => Self::external()
+                .propose()
+                .set_active_proposals_limit(
+                    request.params["active_proposals_limit"]
+                        .as_u64()
+                        .ok_or(Error::msg("active_proposals_limit is required"))?
+                        as u32,
+                )
+                .send(),
+            "DeleteProposal" => Self::external()
+                .propose()
+                .delete(ProposalId(
+                    hex::decode(
+                        request.params["proposal_id"]
+                            .as_str()
+                            .ok_or_else(|| Error::msg("proposal_id is required"))?,
+                    )?
+                    .try_into()
+                    .map_err(|_| Error::msg("Invalid proposal ID length"))?,
+                ))
+                .send(),
+            _ => return Err(Error::msg("Invalid action type")),
+        };
+
+        env::emit(&Event::ProposalCreated { id: proposal_id });
+
+        let old = self.messages.insert(proposal_id, Vector::new())?;
+        if old.is_some() {
+            return Err(Error::msg("proposal already exists"));
+        }
+
+        Ok(proposal_id)
+    }
+
+    pub fn approve_proposal(&self, proposal_id: ProposalId) -> Result<(), Error> {
+        // fixme: should we need to check this?
+        // self.messages
+        //     .get(&proposal_id)?
+        //     .ok_or(Error::msg("proposal not found"))?;
+
+        Self::external().approve(proposal_id);
+
+        env::emit(&Event::ApprovedProposal { id: proposal_id });
+
+        Ok(())
+    }
+
+    pub fn get_proposal_messages(&self, proposal_id: ProposalId) -> Result<Vec<Message>, Error> {
+        let Some(msgs) = self.messages.get(&proposal_id)? else {
+            return Ok(vec![]);
+        };
+
+        let entries = msgs.entries()?;
+
+        Ok(entries.collect())
+    }
+
+    pub fn send_proposal_messages(
+        &mut self,
+        proposal_id: ProposalId,
+        message: Message,
+    ) -> Result<(), Error> {
+        let mut messages = self.messages.get(&proposal_id)?.unwrap_or_default();
+
+        messages.push(message)?;
+
+        self.messages.insert(proposal_id, messages)?;
+
+        Ok(())
+    }
+}
+
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+    
+//     // Helper function to setup test environment
+//     fn setup() -> AppState {
+//         AppState::init()
+//     }
+
+//     #[test]
+//     fn test_currently_active_player() {
+//         let state = setup();
+//         assert_eq!(state.active_player, 0); // Assuming default is 0
+        
+//     }
+// }
